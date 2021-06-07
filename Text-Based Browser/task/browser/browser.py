c2b74d031bf400f604cf8aa16673f4fb8a7b485e
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-06-07 18:43:34 +0100

    Update browser.py

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -1,5 +1,5 @@
-# Text-Based Browser
-# Author: Jason Tolbert (https://github.com/jasonalantolbert)
+# Squirrel-Navigator
+# Author: squirrelcom (https://github.com/squirrelcom)
 # Python Version: 3.8
 
 
@@ -17,16 +17,15 @@ from bs4 import BeautifulSoup
 
 
 def browse(tab_directory):  # contains all functions of the browser
-    history = deque()  # creates a deque to hold the browser history
+    history = deque()
     while True:
-        # gets a url from the user, stripping the protocol identifier if present
-        url = re.sub("^http[s]?[:]//", "", input("\nEnter a URL to browse, 'back' to go back, or 'exit' to exit:\n"))
+        url = re.sub("^http[s]?[:]//", "", input("\nWelcome to Squirrel-Navigator!! Enter a URL to browse, 'back' to go back, or 'exit' to exit:\n"))
 
         if url == "exit":
             exit()
-        elif url == "back":  # goes back to the previous page
+        elif url == "back":
             try:
-                history[-2]  # checks if there's a previous page to go back to
+                history[-2]
             except IndexError:
                 url = None  # if there isn't, the url is set to None
             else:  # if there is, the url is set to penultimate value in history
@@ -37,16 +36,8 @@ def browse(tab_directory):  # contains all functions of the browser
             try:
                 site = requests.get(f"https://{url}")
                 soup = BeautifulSoup(site.content, "lxml")
-                # opens (or creates if nonexistent) a new file in the tabs folder with the same name as the url minus
-                # the protocol identifier and top-level-domain (e.g. for the url https://google.com, the file name
-                # would be "google")
-                #
-                # these files function as the browser's tabs and can be opened by simply typing the site name
-                # instead of the url (e.g. once https://google.com has been saved in a tab, it can be loaded again
-                # by typing only "google")
                 with open(f"{tab_directory}/{re.sub('.[a-z]+$', '', url)}", "w+") as site_file:
                     for element in soup.find_all(re.compile("p|a|ul|ol|li|^h[1-6]$")):
-                        # this for loop finds all elements in the page with any of a limited range of HTML tags
                         if element.name == "a":  # makes hyperlink text blue
                             print(colorama.Fore.BLUE + element.get_text())
                             print(colorama.Style.RESET_ALL)
@@ -57,15 +48,12 @@ def browse(tab_directory):  # contains all functions of the browser
                             site_file.write(f"{element.get_text()}\n")
                 history.append(re.sub('.[a-z]+$', '', url))  # appends site name to history
             except BaseException:
-                # trying to open an existing tab will cause an exception
-                # with requests.get(), so if one occurs the program check to see if
-                # there's an existing tab with a name that matches the user input
                 try:
                     with open(f"{tab_directory}/{url}", "r") as site_file:
                         print(site_file.read())
                     history.append(url)
                 except BaseException:  # if no existing tab can be found the user input an error message is printed
-                    print("Error: Incorrect URL")
+                    print("Error: No website with that URL")
 
 
 # command line argument parsing
